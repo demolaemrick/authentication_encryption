da4504a5dcaa71634a10ed3690e60f254daf7caf
Emrick19 <emricky17@gmail.com>
2020-07-03 14:30:48 +0100

    Updated some files

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-require('dotenv').config()
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
 
 const app = express();
 app.use(express.static('public'))
