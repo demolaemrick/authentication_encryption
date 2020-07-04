31e451206ab743bfa86dc174f779b7fb8f945417
Emrick19 <emricky17@gmail.com>
2020-07-04 14:42:29 +0100

    Updated the app.js

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://admin-ademola:"+process.env.DB_PASSWORD+"@cluster0.kcwsn.mongodb.net/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const url = process.env.SECRET_URL
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useCreateIndex', true)
 
 let Schema = mongoose.Schema
